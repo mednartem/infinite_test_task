53cc4212b51be0f50ed8ee01869da06c80d7e3d3
Artem <mednartem@yandex.ru>
2024-01-10 12:52:01 -0800

    add logback dependency, add base url

--- src/test/java/com/infinite/tests/GoogleTest.java
+++ src/test/java/com/infinite/tests/GoogleTest.java
@@ -2,6 +2,7 @@ package com.infinite.tests;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selenide;
+import com.infinite.main.config.ProjectConfig;
 import com.infinite.main.jupiter.WebTest;
 import org.junit.jupiter.api.Test;
 
@@ -13,7 +14,7 @@ public class GoogleTest {
         String searchText = "infinite com";
         String expectedLink = "https://www.infinite.com/";
 
-        Selenide.open("https://google.com");
+        Selenide.open(ProjectConfig.app.baseUrl());
         Selenide.$("[name=q]").setValue(searchText).submit();
         Selenide.$$("#rso a").first().shouldHave(Condition.attribute("href", expectedLink));
     }

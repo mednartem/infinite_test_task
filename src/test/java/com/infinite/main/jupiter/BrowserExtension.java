9c4e5aeb2082a3c4ef4c83a7ec62b983b861bb5f
Artem <mednartem@yandex.ru>
2024-01-10 12:46:31 -0800

    make attach console logs and current url to the failed test

--- src/test/java/com/infinite/main/jupiter/BrowserExtension.java
+++ src/test/java/com/infinite/main/jupiter/BrowserExtension.java
@@ -1,14 +1,16 @@
 package com.infinite.main.jupiter;
 
 import com.codeborne.selenide.WebDriverRunner;
+import com.infinite.main.attachments.AttachmentHelpers;
 import com.infinite.main.driver.DesktopDriver;
 import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.TestExecutionExceptionHandler;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
 
-public class BrowserExtension implements BeforeEachCallback, AfterEachCallback {
+public class BrowserExtension implements BeforeEachCallback, AfterEachCallback, TestExecutionExceptionHandler {
 
     @Override
     public void beforeEach(ExtensionContext context) {
@@ -20,5 +22,14 @@ public class BrowserExtension implements BeforeEachCallback, AfterEachCallback {
         if (WebDriverRunner.hasWebDriverStarted()) {
             closeWebDriver();
         }
+    }
+
+    @Override
+    public void handleTestExecutionException(ExtensionContext context, Throwable throwable) throws Throwable {
+        if (WebDriverRunner.hasWebDriverStarted()) {
+            AttachmentHelpers.attachAsText("Current url", WebDriverRunner.getWebDriver().getCurrentUrl());
+            AttachmentHelpers.attachAsText("Browser console logs", AttachmentHelpers.getConsoleLogs());
+        }
+        throw throwable;
     }
 }

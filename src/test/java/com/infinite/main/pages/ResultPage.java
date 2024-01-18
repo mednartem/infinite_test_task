36b487db8cad80a66128844a9c15bd507b860833
Artem <mednartem@yandex.ru>
2024-01-18 14:19:33 -0800

    adapted pages for possible Engines

--- src/test/java/com/infinite/main/pages/ResultPage.java
+++ src/test/java/com/infinite/main/pages/ResultPage.java
@@ -1,16 +1,28 @@
 package com.infinite.main.pages;
 
+import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.ElementsCollection;
+import com.infinite.main.dictionary.Engine;
 import io.qameta.allure.Step;
 
-import static com.codeborne.selenide.Condition.href;
 import static com.codeborne.selenide.Selenide.$$;
 
 public class ResultPage {
-    private final ElementsCollection listResults = $$("#search a");
+    @Step("Verify that at least one link on the page matches the expected link '{expectedLink}' in the '{engine}' search")
+    public ResultPage atLeastOneLinkOnThePageShouldBe(Engine engine, String expectedLink) {
+        switch (engine) {
+            case GOOGLE -> atLeastOneLinkOnThePageShouldBe($$("#search a"), expectedLink);
+            case YAHOO -> atLeastOneLinkOnThePageShouldBe($$("#web a"), expectedLink);
+            case YANDEX -> atLeastOneLinkOnThePageShouldBe($$("#search-result a"), expectedLink);
+            default -> throw new IllegalStateException("Unexpected value: " + engine);
+        }
+        return this;
+    }
 
-    @Step("I check first link should be `{expectedLink}`")
-    public void firstLinkShouldBe(String expectedLink) {
-        listResults.first().shouldHave(href(expectedLink));
+    private void atLeastOneLinkOnThePageShouldBe(ElementsCollection elements, String expectedLink) {
+        elements.shouldHave(CollectionCondition.anyMatch(
+                "At least one link on the page should be: " + expectedLink,
+                el -> el.getAttribute("href").equals(expectedLink)
+        ));
     }
 }
--- src/test/java/com/infinite/main/pages/SearchPage.java
+++ src/test/java/com/infinite/main/pages/SearchPage.java
@@ -1,16 +1,25 @@
 package com.infinite.main.pages;
 
 import com.codeborne.selenide.SelenideElement;
+import com.infinite.main.dictionary.Engine;
 import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class SearchPage {
-    private final SelenideElement searchInput = $("[name=q]");
 
-    @Step("I enter value `{text}` to the search bar and press Enter")
-    public ResultPage enterValueToTheSearchBarAndPressEnter(String text) {
-        searchInput.setValue(text).pressEnter();
-        return new ResultPage();
+    @Step("I input `{text}` into the search bar and hit Enter using the `{engine}`")
+    public SearchPage enterValueToSearchBarAndPressEnter(Engine engine, String text) {
+        switch (engine) {
+            case GOOGLE -> enterValueToSearchBarAndPressEnter($("[name=q]"), text);
+            case YAHOO -> enterValueToSearchBarAndPressEnter($("[name=p]"), text);
+            case YANDEX -> enterValueToSearchBarAndPressEnter($("[name=text]"), text);
+            default -> throw new IllegalStateException("Unexpected value: " + engine);
+        }
+        return this;
+    }
+
+    private void enterValueToSearchBarAndPressEnter(SelenideElement element, String text) {
+        element.setValue(text).pressEnter();
     }
 }
